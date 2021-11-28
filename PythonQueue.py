4c1f4b3980ea610c8351d58488718934aaaf3c8c
jkog77 <71049439+jkog77@users.noreply.github.com>
2021-11-28 15:54:15 -0500

    Python Queue

---
+++ PythonQueue.py
@@ -0,0 +1,12 @@
+acceptedName = ['Adam', 'Bill', 'Carl', 'David', 'Eric', 'Frank', 'George', 'Howard', 'Ian', 'Jack']
+names = []
+names.append(acceptedName)
+
+for x in names:
+    print(x)
+
+
+names = ['Adam', 'Bill', 'Carl', 'David', 'Eric', 'Frank', 'George', 'Howard', 'Ian', 'Jack']
+
+while len(names) > 0:
+    print(names.pop(0))
